4fdf173f3a37caed71b7c24921bbb33cf5797ee1
erolanda <elmaildemlanda@gmail.com>
2016-09-29 14:08:59 -0500

    added example with a PyQt GUI

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
     # Display the resulting frame
     cv2.imshow('image',gray)
